d490b91641d2757386a70d9408560674edd330ca
Donata Malecka <niphree@gmail.com>
2011-09-21 20:26:41 +0200

    sync

--- workspace/magisterka/src/ir/crawler/Crawlers.java
+++ workspace/magisterka/src/ir/crawler/Crawlers.java
@@ -6,7 +6,7 @@ public class Crawlers {
 		
 		//DataBaseUpdate update_tags_cache = new DataBaseUpdate();
 		//update_tags_cache.start();
-		
+		/*
 		boolean recreate = false;
 		DeliciousFeedCrawler crawler = new DeliciousFeedCrawler(recreate, CrawlerType.TOP);
 		crawler.start();
@@ -14,18 +14,18 @@ public class Crawlers {
 		DeliciousFeedCrawler crawler2 = new DeliciousFeedCrawler(false, CrawlerType.NEW, "http://feeds.delicious.com/v2/rss/recent?min=20&count=100");
 		crawler2.start();
 		System.out.println("WAITING FOR OTHER CRAWLERS TO START");
-		
+		*/
 		
 		//http://feeds.delicious.com/v2/rss/recent?min=20&count=100
 		//sleep(60*1000);
-	/*	
+		
 		System.out.println("STARTING OTHER CRAWLERS");
 		TwitterCrawler twitter = new TwitterCrawler();
 		twitter.start();
 		DiggCrawler digg = new DiggCrawler();
 		digg.start();
 		FacebookCrawler facebook = new FacebookCrawler();
-		facebook.start();*/
+		facebook.start();
 		
 	}
 	
--- workspace/magisterka/src/ir/crawler/DataBaseUpdate.java
+++ workspace/magisterka/src/ir/crawler/DataBaseUpdate.java
@@ -29,9 +29,14 @@ public class DataBaseUpdate extends Thread{   //this should be thread
 		Transaction tx = session.beginTransaction();
 		
 		List<DocumentTable> docs = session.createQuery("from DocumentTable m").list();
+		tx.commit();
+		session.close();
 		
-		
-		for (DocumentTable doc : docs){
+		for (DocumentTable doc1 : docs){
+			 session = HibernateUtil.getSession();
+			 tx = session.beginTransaction();
+			 
+			DocumentTable doc = (DocumentTable)session.get(DocumentTable.class, doc1.getId());
 			Query users_q = session.createQuery("select doc.id, doc.url, count(distinct u.id), doc.facebook_value, doc.digg_value, doc.twitter_value " +
 					"from UserTagDocTable utd join utd.user u join utd.doc doc " +
 					"where doc.id = ? " )
@@ -43,27 +48,44 @@ public class DataBaseUpdate extends Thread{   //this should be thread
 				"from UserTagDocTable utd join utd.tags tags join utd.doc doc " +
 				"where doc.id = ? " +
 				"group by tags.id " +
-				"order by c")
+				"order by c desc")
 				.setMaxResults(10).setLong(0, doc.getId());
-			
+
 			String res = "";
-			res += " <B>url:</B> " + users.get(0)[1];
-			res += "<BR><B>users:</B> " + users.get(0)[2];
-			res += " <B>facebook:</B> " + users.get(0)[3];
-			res += " <B>digg:</B> "  + users.get(0)[4];
-			res += " <B>twitter:</B> " + users.get(0)[5];
+			res="<div class='results-search-block'>" +
+					"<div class='results-search-title'>" +
+					"<a href='"+users.get(0)[1]+"'>"+users.get(0)[1]+
+                    " </a>"+
+                    "</div>"+
+                    "<div class='results-search-link'>"+
+                    users.get(0)[1]+"</div>"+
+                    "<div class='results-search-text'> %s "+
+                    "</div>"+
+                    "<div class='results-search-tags'>";
 			
-			res += "<BR><b>TAGS: </b> ";
 			List<Object[]> tags = q.list();
 			for (Object[] tag : tags){
-				res += "[ " + tag[1] +" x" + tag[2]+ "]";
+				res += "<a> " + tag[1] +" x" + tag[2]+ "</a>";
 			}
+			res +="</div>";
+			res +="<div class='results-search-services'>";
+			res +=("<a><b>added by:</b> "+ users.get(0)[2]+"</a> " +
+				   "<a><b>facebook:</b> "+ users.get(0)[3]+" </a> " +
+				   "<a><b>digg:</b> "+users.get(0)[4]+" </a>" +
+                   "<a><b>twitter:</b>" + users.get(0)[5]+ "</a>");
+			res +="</div>";
+			res +="</div>";
 			
 			
+			//System.out.println(res);
 			doc.setTags_cashe1(res);
+			System.out.println(doc.getId());
 			session.save(doc);
+			tx.commit();
+			session.close();
 			
 		}
+		
 		tx.commit();
 		session.close();
 		System.out.println("Ending Document table cache update. going to sleep. Goodnight.");
